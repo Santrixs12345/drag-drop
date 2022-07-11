8c994288e361ee0aa2108625e381ca249987cab3
Santrixs12345 <santiago.vaca@uniat.edu.mx>
2022-07-11 09:41:10 -0500

    first commit

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react';
+import Lane from '../../components/Lane/Lane';
+import './Board.css';
+
+const lanes = [
+    {id: 1, title: 'To Do'},
+    {id: 2, title: 'In Progress'},
+    {id: 3, title: 'Review'},
+    {id: 4, title: 'Done'},
+]
+
+function Board(props){
+    const [loading,setLoading] = useState(false);
+    const [tasks,setTasks] = useState([]);
+    const [error,setError] = useState();
+
+    useEffect(() => {
+        async function fetchData(){
+            try{
+                const tasks = await fetch('https://my-json-server.typicode.com/PaulAnds1/my-api/tasks');
+                const result = await tasks.json();
+
+                if(result){
+                    setTasks(result);
+                    setLoading(false);
+                }
+
+            } catch(e){
+                setLoading(false);
+                setError(e.error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+
+    return(
+        <div className= 'Board-wrapper'>
+            {
+                lanes.map((lane)=>(
+                    <Lane 
+                    key={lane.id} 
+                    title={lane.title}
+                    loading={loading}
+                    error={error}
+                    tasks={tasks.filter((task)=>task.lane === lane.id)}
+                    />
+                ))
+            }
+        </div>
+    )
+}
+
+export default Board;
